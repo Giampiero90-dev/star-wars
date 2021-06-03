222111fec7ff0b662ca96e83074dae3359bf5c16
Giampiero90-dev <giampiero.devita@hotmail.it>
2021-06-03 09:34:51 +0200

    Setup search and sort inputs features

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import { fetchPeople } from "./store/people/actions";
@@ -6,9 +6,11 @@ import { selectPeople } from "./store/people/selectors";
 
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 
 function App() {
+  const [searchInput, set_searchInput] = useState("");
+  const [selectedSortingMethod, setSelectedSortingMethod] = useState("name");
   const dispatch = useDispatch();
   const people = useSelector(selectPeople);
 
@@ -24,7 +26,28 @@ function App() {
           alt="logo"
           className="logo"
         ></img>
-        <Row className="actionsContainer"></Row>
+        <Row className="actionsContainer">
+          <Col>
+            <input
+              className="searchBar"
+              value={searchInput}
+              onInput={(e) => set_searchInput(e.target.value)}
+              placeholder="Search..."
+            />
+          </Col>
+          <Col>
+            <select
+              className="searchBar"
+              placeholder="Sort"
+              value={selectedSortingMethod}
+              onChange={(e) => setSelectedSortingMethod(e.target.value)}
+            >
+              <option value="name">Name</option>
+              <option value="height">Height</option>
+              <option value="mass">Mass</option>
+            </select>
+          </Col>
+        </Row>
       </Container>
     </div>
   );

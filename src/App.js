6f544a731c4b0bf3ed188644b1e66eeb17cd9be4
Giampiero90-dev <giampiero.devita@hotmail.it>
2021-06-03 09:30:27 +0200

    fetch data on Redux working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { fetchPeople } from "./store/people/actions";
+import { selectPeople } from "./store/people/selectors";
+
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row } from "react-bootstrap";
 
 function App() {
+  const dispatch = useDispatch();
+  const people = useSelector(selectPeople);
+
+  useEffect(() => {
+    dispatch(fetchPeople());
+  }, [dispatch]);
+
   return (
     <div className="App">
       <Container className="mainContainer">
